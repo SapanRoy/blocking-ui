e1677f3c76d2bf014b3fcd39a91ef31ed72747d1
SapanRoy <isapanroy@gmail.com>
2021-01-17 20:58:17 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  title = 'my-web-worker-app';
+  selectedColour: string;
+  result: any;
+  colourArr = ['#9999ff', '#00aaff', '#008000', '#b33c00', '#663300', '#cc3399'];
+  isBusy = false;
+  cpuWorker: any;
+
+  constructor() {
+  }
+
+  changeColor(color: string) {
+    this.selectedColour = color;
+  }
+
+  cpuIntensiveWork() {
+    this.isBusy = true;
+    const start = new Date().getTime();
+    let calResult = 0;
+    for (let i = Math.pow(environment.baseNumber, 7); i >= 0; i--) {
+      calResult += Math.atan(i) * Math.tan(i);
+    };
+
+    let elapsed = new Date().getTime() - start;
+    const milliseconds = (elapsed % 1000) / 100;
+    const seconds = Math.floor((elapsed / 1000) % 60);
+    const minutes = Math.floor((elapsed / (1000 * 60)) % 60);
+    this.result = { 'TimeElapsed': `${minutes}m, ${seconds}s, ${milliseconds}ms`, 'output': calResult };
+    this.isBusy = false;
+  }
+}
